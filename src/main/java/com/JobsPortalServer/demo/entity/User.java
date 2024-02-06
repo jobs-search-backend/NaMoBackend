8f848e57ec9814c0633feb89e182ac7b3e6b7e35
phoenix600 <pranayramteke613@gmail.com>
2024-02-06 07:17:48 -0800

    NAMO portal

---
+++ src/main/java/com/JobsPortalServer/demo/entity/User.java
@@ -0,0 +1,15 @@
+package com.JobsPortalServer.demo.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+}
